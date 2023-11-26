fec5ab827ae12e6ec55f13a2d563163b8cfa4e23
Lukasz <lukaszczech98@gmail.com>
2023-11-26 20:59:42 +0100

    Added simple views

--- vehicles/urls.py
+++ vehicles/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import VehicleListView
+from .views import DashboardView, VehicleListView, VehicleTypeListView, VehicleStatusListView
 
 urlpatterns = [
-    path('vehicles/', VehicleListView.as_view(), name='vehicle_list'),
+    path('dashboard', DashboardView.as_view(), name='dashboard'),
+    path('list', VehicleListView.as_view(), name='vehicle_list'),
+    path('types', VehicleTypeListView.as_view(), name='vehicle_types_list'),
+    path('statuses', VehicleStatusListView.as_view(), name='vehicle_statuses_list'),
 ]
--- vehicles/views.py
+++ vehicles/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
 from django.views import View
-from .models import Vehicle
+from .models import Vehicle, VehicleType, VehicleStatus
 
 
 # Create your views here.
+class DashboardView(View):
+    template_name = 'vehicles/dashboard.html'
+
+    def get(self, request, *args, **kwargs):
+        # Example data for the dashboard
+        total_vehicles = Vehicle.objects.count()
+        vehicle_types = VehicleType.objects.all()
+        vehicle_statuses = VehicleStatus.objects.all()
+
+        # Pass the data to the template
+        context = {
+            'total_vehicles': total_vehicles,
+            'vehicle_types': vehicle_types,
+            'vehicle_statuses': vehicle_statuses,
+        }
+        return render(request, self.template_name, context)
 class VehicleListView(View):
     template_name = 'vehicles/vehicle_list.html'
 
@@ -13,4 +29,28 @@ class VehicleListView(View):
 
         # Pass the list of vehicles to the template
         context = {'vehicles': vehicles}
+        return render(request, self.template_name, context)
+
+
+class VehicleTypeListView(View):
+    template_name = 'vehicles/vehicle_type_list.html'
+
+    def get(self, request, *args, **kwargs):
+        # Retrieve the list of vehicle types from the database
+        vehicle_types = VehicleType.objects.all()
+
+        # Pass the list of vehicle types to the template
+        context = {'vehicle_types': vehicle_types}
+        return render(request, self.template_name, context)
+
+
+class VehicleStatusListView(View):
+    template_name = 'vehicles/vehicle_status_list.html'
+
+    def get(self, request, *args, **kwargs):
+        # Retrieve the list of vehicle statuses from the database
+        vehicle_statuses = VehicleStatus.objects.all()
+
+        # Pass the list of vehicle statuses to the template
+        context = {'vehicle_statuses': vehicle_statuses}
         return render(request, self.template_name, context)
