732f39b43aae5dfa27129e27768230c3ef20e929
matid64 <matid1996@gmail.com>
2024-01-03 05:54:17 +0100

    Styled lists and Modals for vehicle, vehicle type , vehicle status , user

--- vehicles/urls.py
+++ vehicles/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-from .views import DashboardView, VehicleListView, VehicleTypeListView, VehicleStatusListView
+from .views import DashboardView, VehicleListView, VehicleTypeListView, VehicleStatusListView,UsersListView,ServicesListView
 
 urlpatterns = [
     path('dashboard', DashboardView.as_view(), name='dashboard'),
     path('list', VehicleListView.as_view(), name='vehicle_list'),
+    path('vehicle', VehicleListView.as_view(), name='vehicle_details'),
     path('types', VehicleTypeListView.as_view(), name='vehicle_types_list'),
     path('statuses', VehicleStatusListView.as_view(), name='vehicle_statuses_list'),
+    path('services', ServicesListView.as_view(), name='services_list'),
+    path('service', ServicesListView.as_view(), name='service_details'),
+    path('users', UsersListView.as_view(), name='users_list'),
+    path('user', UsersListView.as_view(), name='users_details'),
 ]
--- vehicles/views.py
+++ vehicles/views.py
@@ -54,3 +54,38 @@ class VehicleStatusListView(View):
         # Pass the list of vehicle statuses to the template
         context = {'vehicle_statuses': vehicle_statuses}
         return render(request, self.template_name, context)
+
+class ServicesListView(View):
+    template_name = 'vehicles/services_list.html'
+
+    def get(self, request, *args, **kwargs):
+        # Retrieve the list of vehicle statuses from the database
+        return render(request, self.template_name)
+
+class UsersListView(View):
+    template_name = 'vehicles/users_list.html'
+
+    def get(self, request, *args, **kwargs):
+        # Retrieve the list of vehicle statuses from the database
+        return render(request, self.template_name)
+
+class ServiceDetailsView(View):
+    template_name = 'vehicles/service_details.html'
+
+    def get(self, request, *args, **kwargs):
+        # Retrieve the list of vehicle statuses from the database
+        return render(request, self.template_name)
+
+class UserDetailsView(View):
+    template_name = 'vehicles/user_details.html'
+
+    def get(self, request, *args, **kwargs):
+        # Retrieve the list of vehicle statuses from the database
+        return render(request, self.template_name)
+
+class VehicleDetailsView(View):
+    template_name = 'vehicles/vehicle_details.html'
+
+    def get(self, request, *args, **kwargs):
+        # Retrieve the list of vehicle statuses from the database
+        return render(request, self.template_name)
